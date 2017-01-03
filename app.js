99161b7cf8877c0278ec23800787f8f6b63c4432
numbergames <bfletch@gmail.com>
2017-01-03 12:00:07 -0500

    finish morning exercises

---
+++ app.js
@@ -0,0 +1,66 @@
+'use strict';
+
+// # 1
+function oddOrEven(num) {
+
+  if (num < 1) {
+    return 'invalid input';
+  }
+
+  if (num === 1) {
+    return 'odd';
+  }
+
+  if (num === 2) {
+    return 'even';
+  }
+
+  return oddOrEven(num - 2);
+
+}
+
+// # 2
+function arrayDoubler(arr) {
+
+  if (arr.length === 1) {
+    return [arr[0] * 2];
+  }
+
+  return [arr[0] * 2, ...arrayDoubler(arr.slice(1))];
+}
+
+// # 3
+function stringReverse(str) {
+
+  if (!str.length) {
+    return "";
+  }
+
+  return str[str.length - 1] + stringReverse(str.slice(0, str.length - 1));
+
+}
+
+// # 4
+function nthTriangular(n) {
+
+  if (n === 0) {
+    return 0;
+  }
+
+  return n + nthTriangular(n - 1);
+
+}
+
+// # 5
+// "hello", "e" => "h|e|llo"
+function stringSplit(str, separator) {
+
+  if (!str.length) {
+    return "";
+  }
+
+  return (str[0] === separator
+      ? `,`
+      : str[0]) +
+    stringSplit(str.slice(1), separator);
+}
---
+++ readme.md
@@ -0,0 +1,9 @@
+- Exercise 1: Take an integer as input, and return a boolean indicating whether the value is even or odd.
+
+- Exercise 2: Take an array as input which contains an unknown set of numbers, and output an array which doubles the values of each item in that array. Test your solution by trying a handful of different arrays. Don't worry about error checking to make sure that the array you're given is valid input.
+
+- Exercise 3: Take a string as input, reverse the string, and return the new string.
+
+- Exercise 4: Calculates the nth [triangular number](https://en.wikipedia.org/wiki/Triangular_number).
+
+- Exercise 5: Split a string based upon a separator (similar to [String.prototype.split](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split)).
